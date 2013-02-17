4ec2d1fbce188f264f54de36991f6ebf2648f66f
Jonathan Calvert <jcalvert@taximagic.com>
2013-02-17 17:08:09 +0000

    Add delegator class

--- lib/jruby-poi.rb
+++ lib/jruby-poi.rb
@@ -12,3 +12,24 @@ module JrubyPoi
 end
 
 JrubyPoi.load_jars!
+java_import org.apache.poi.hwpf.HWPFDocument
+java_import org.apache.poi.poifs.filesystem.POIFSFileSystem
+java_import java.io.FileInputStream
+
+
+module JrubyPoi
+	class Document < Delegator
+		def initialize(path)
+			fis = Java::OrgApachePoiPoifsFilesystem::POIFSFileSystem.new(Java::JavaIo::FileInputStream.new(path))
+			@doc = Java::OrgApachePoiHwpf::HWPFDocument.new(fs);
+		end
+
+		def __getobj__
+			@doc
+		end
+		
+		def __setobj__(obj)
+			@doc = obj
+		end
+	end
+end
